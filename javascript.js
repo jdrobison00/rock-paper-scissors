37a5976fb8172be68cf66b8755ea39476f1718c5
JD Robison <jdrobison00@gmail.com>
2025-11-30 23:22:31 -0500

    Rework functionality for RPS game with DOM Manipulation.

--- javascript.js
+++ javascript.js
@@ -19,59 +19,85 @@ function getHumanChoice() {
     return prompt('Please enter "rock", "paper", or "scissors".');
 }
 
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        // declare output string, initially empty
-        let winString = "";
-        // determine winner and return corresponding string
-        if (humanChoice === computerChoice) {
-            // condition for tie.
-            winString = "Tie! Both chose " + humanChoice + ".";
-        } else if (humanChoice === "rock") { // conditions for non-tie human choice as rock
-            if (computerChoice === "paper") {
-                winString = "You lose! Paper beats rock.";
-                compScore += 1;
-            } else {
-                winString = "You win! Rock beats scissors.";
-                humanScore += 1;
-            }
-        } else if (humanChoice === "paper") { // conditions for non-tie human choice as paper
-            if (computerChoice === "rock") {
-                winString = "You win! Paper beats rock.";
-                humanScore += 1;
-            } else {
-                winString = "You lose! Scissors beats paper.";
-                compScore += 1;
-            }
-        } else { // conditions for non-tie human choice as scissors
-            if (computerChoice === "rock") {
-                winString = "You lose! Rock beats scissors.";
-                compScore += 1;
-            } else {
-                winString = "You win! Scissors beats paper.";
-                humanScore += 1;
-            }
+let roundCount = 0;
+let compScore = 0;
+let humanScore = 0;
+
+let youResult = document.querySelector(".human-score");
+let compResult = document.querySelector(".comp-score");
+let results = document.querySelector(".results");
+
+let youText = document.createElement('span');
+youResult.appendChild(youText);
+
+let compText = document.createElement('span');
+compResult.appendChild(compText);
+
+
+
+function playRound(humanChoice, computerChoice) {
+    if( humanScore >= 5 || compScore >= 5 ) {
+        return;
+    }
+
+    roundCount += 1;
+    // declare output string, initially empty
+    let winString = "";
+    // determine winner and return corresponding string
+    if (humanChoice === computerChoice) {
+        // condition for tie.
+        winString = "Tie! Both chose " + humanChoice + ".";
+    } else if (humanChoice === "rock") { // conditions for non-tie human choice as rock
+        if (computerChoice === "paper") {
+            winString = "You lose! Paper beats rock.";
+            compScore += 1;
+        } else {
+            winString = "You win! Rock beats scissors.";
+            humanScore += 1;
         }
-
-        console.log(winString);
+    } else if (humanChoice === "paper") { // conditions for non-tie human choice as paper
+        if (computerChoice === "rock") {
+            winString = "You win! Paper beats rock.";
+            humanScore += 1;
+        } else {
+            winString = "You lose! Scissors beats paper.";
+            compScore += 1;
+        }
+    } else { // conditions for non-tie human choice as scissors
+        if (computerChoice === "rock") {
+            winString = "You lose! Rock beats scissors.";
+            compScore += 1;
+        } else {
+            winString = "You win! Scissors beats paper.";
+            humanScore += 1;
+        }
     }
 
-    //create a number variable to represent human score with initial value of 0.
-    let humanScore = 0;
-    //create a number variable to represent computer score with initial value of 0.
-    let compScore = 0;
+    youText.textContent = `${humanScore}`;
+    compText.textContent = `${compScore}`;
 
-    playRound(getHumanChoice(), getComputerChoice());
+    let newMessage = document.createElement('p');
+    newMessage.textContent = "Round " + roundCount + ": " + winString;
+    results.appendChild(newMessage);
 
-    if (humanScore > compScore) {
-        console.log("Congratulations! You won the whole game!");
-    } else if (compScore > humanScore) {
-        console.log("The computer one this game. Better luck next time!");
-    } else {
-        console.log("Tie overall!");
+    let winMessage = document.createElement('p');
+    if (humanScore >= 5) {
+        winMessage.textContent = "Game over! You win!";
     }
+    if (compScore >= 5) {
+        winMessage.textContent = "Game over! Computer wins!"
+    }
+    results.appendChild(winMessage);
 }
 
-playGame();
+let rockButton = document.querySelector("#rock");
+rockButton.addEventListener('click', () => {playRound("rock", getComputerChoice())});
+
+let paperButton = document.querySelector("#paper");
+paperButton.addEventListener('click', () => {playRound("paper", getComputerChoice())});
+
+let scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener('click', () => {playRound("scissors", getComputerChoice())});
+
 
 
